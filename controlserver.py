4c5e604b929211ea7a4874b1fb3a6f176fe2b826
Punit Soni <punitxsmart@gmail.com>
2013-11-03 01:14:59 -0700

    more code for message framing

--- controlserver.py
+++ controlserver.py
@@ -10,17 +10,20 @@ import logging
 
 # setup logging for module
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(levelname)s %(name)s:%(message)s')
 ch.setFormatter(formatter)
+logger.addHandler(ch)
 
-def setLogLevel(level):
+def setLoglevel(level):
     logger.setLevel(level)
 
 EVENT_SERVER_FINISHED = 0
 EVENT_SERVER_TIMEDOUT = 1
+EVENT_NEW_MSG = 2
+
 
 class ControlServer():
     'Listens to control commands from clients'
@@ -39,11 +42,11 @@ class ControlServer():
         self.clientAddr = None
         self.cLock = threading.Lock()
         self.eventCb = None
-        self.dataCb = None
 
     def openSocket(self):
         try:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server.bind((self.host,self.port))
             self.server.listen(5)
         except socket.error, (value,message):
@@ -53,15 +56,13 @@ class ControlServer():
             sys.exit(1)
 
 
-    def start(self, dataCb, eventCb):
+    def start(self, eventCb):
         logger.info("starting server thread")
-        self.dataCb = dataCb
         self.eventCb = eventCb
         t = threading.Thread(target=self.run)
-#        t.daemon = True
         t.start()
 
-    def sendDataToClient(self, data):
+    def sendDataToClient(self, data=None):
         if self.numClients < 1:
             self.logging.error("no clients alive")
             return
@@ -73,9 +74,9 @@ class ControlServer():
         logger.info("stopping server")
         self.running = False
 
-    def sendEvent(self, event):
+    def sendEventCb(self, event, data):
         if self.eventCb:
-            self.eventCb(event, None)
+            self.eventCb(event, data)
 
     def decodeFrameLen(self, lenInfo):
         len = (ord(lenInfo[0]) + (ord(lenInfo[1]) << 8) +
@@ -84,14 +85,14 @@ class ControlServer():
 
     def recvFrameMessage(self, s):
         recv_count = 4;
-        data = s.recv(recv_count).decode("ascii")
-        if !data:
+        data = s.recv(recv_count)#.decode("ascii")
+        if data == 0:
             return None
         total_rx = len(data)
         lenInfo = data
         while total_rx < recv_count:
-            data = s.recv(recv_count - total_rx).decode("ascii")
-            if !data:
+            data = s.recv(recv_count - total_rx)#.decode("ascii")
+            if data == 0:
                 return None
             total_rx += len(data)
             lenInfo = lenInfo + data
@@ -103,15 +104,15 @@ class ControlServer():
         total_rx = len(data)
         msg = data
         while total_rx < recv_count:
-            data = s.recv(recv_count - total_rx).decode("ascii")
-            if !data:
+            data = s.recv(recv_count - total_rx)#.decode("ascii")
+            if data == 0:
                 return None            
             total_rx += len(data)
             msg = msg + data
         logger.info("msg = " + msg)
-        
+        for i in range(0, len(msg)-1):
+        	logger.info("msg[%d] = %s" % (i, msg[i]))
         return msg
-        
         
     def run(self):
         self.openSocket()
@@ -135,35 +136,32 @@ class ControlServer():
                     self.sendDataToClient("cmd>")
                 else:
                     msg = self.recvFrameMessage(s)
-                    #data = s.recv(self.size)
-                    #if data:
-                    #    self.processClientData(data)
-                    #else:
-                    #    s.close()
-                    #    input.remove(s)
-                    #    logger.info("client removed")
-                    #    self.numClients -= 1
+                    if msg:
+                        self.sendEventCb(EVENT_NEW_MSG, msg)
+                    else:
+                        s.close()
+                        input.remove(s)
+                        logger.info("client removed")
+                        self.numClients -= 1
         self.server.close()
         logger.info("server finished")
-        self.sendEvent(EVENT_SERVER_FINISHED)
+        self.sendEventCb(EVENT_SERVER_FINISHED)
 
 ## Module testing code ##
 if __name__ == "__main__":
     print "Press Enter to Exit"
     server = None
-    def dataCallback(key, val):
-        if key == "kill":
-            server.stop()
-        print "key=%s, val=%s" % (key, val)
-
     def eventCallback(event, data):
         print "event = " + str(event)
         if(event == EVENT_SERVER_FINISHED):
             print "server finished, exiting"
             os._exit(0)
+    	elif event == EVENT_NEW_MSG:
+    		print "new_msg = ", data
 
     server = ControlServer()
-    server.start(dataCallback, eventCallback)
+    setLoglevel(logging.INFO)
+    server.start(eventCallback)
     while True:
         input = [sys.stdin]
         inputready,outputready,exceptready = select.select(input,[],[])
--- rv_main.py
+++ rv_main.py
@@ -1,3 +1,48 @@
 #!/usr/bin/python
 
+import logging
+import controlserver
+import rv_protocol
+import sys
+import select
+import os
+
+# setup logging for module
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(levelname)s %(name)s:%(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+def handleNewMsg(msg):
+	msg_dict = rv_protocol.parseMessage(msg)
+	print "msg = ", msg_dict 
+
+def controlEventCb(event, data):
+	print "event = " + str(event)
+	if(event == controlserver.EVENT_SERVER_FINISHED):
+		print "server finished, exiting"
+		os._exit(0)
+	elif event == controlserver.EVENT_NEW_MSG:
+		print "new_msg = ", data
+		handleNewMsg(data)
+
+# main #
+controlserver.setLoglevel(logging.INFO)
+server = controlserver.ControlServer()
+server.start(controlEventCb)
+
+while True:
+        input = [sys.stdin]
+        inputready,outputready,exceptready = select.select(input,[],[])
+        for s in inputready:
+            if s == sys.stdin:
+                junk = sys.stdin.readline()
+                print "BYE"
+                os._exit(0)
+
+
+
 
