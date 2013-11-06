01abbcc6b2e58f0d54867e007da49edc3ec1b743
Punit Soni <punitxsmart@gmail.com>
2013-11-05 18:33:34 -0800

    make rv_main object oriented
    also add gitignore file

--- rv_main.py
+++ rv_main.py
@@ -3,9 +3,6 @@
 import logging
 import ControlServer
 import rv_protocol
-import sys
-import select
-import os
 
 # setup logging for module
 logger = logging.getLogger(__name__)
@@ -18,6 +15,12 @@ logger.addHandler(ch)
 
 class RvController(ControlServer.MsgListener):
 
+    def __init__(self):
+        self.server = ControlServer.Server(self)
+    
+    def run(self):
+        self.server.serveForever()
+    
     def setRoverSpeed(self, ls, rs):
         print "setting speed, %d %d" % (ls, rs)
         
@@ -29,14 +32,16 @@ class RvController(ControlServer.MsgListener):
         print "cmd_id = ", cmd_id
         if cmd_id == rv_protocol.CMD_ID_RV_SPEED:
             [ls, rs] = cmd_dict[rv_protocol.KEY_RV_SPEED]
-            setRoverSpeed(ls, rs)
+            self.setRoverSpeed(ls, rs)
         elif cmd_id == rv_protocol.CMD_ID_SERVO_POS:
             [servo_id, pos] = cmd_dict[rv_protocol.KEY_SERVO_POS]
-            setServoPos(servo_id, pos)
+            self.setServoPos(servo_id, pos)
             
     def handleNewMsg(self, msg):
         msg_dict = rv_protocol.parseMessage(msg)
         print "msg_dict = ", msg_dict
+        if msg_dict is None:
+            return
         if msg_dict[rv_protocol.KEY_MSG_TYPE] == rv_protocol.MSG_TYPE_RAW:
             print "raw_message = ", msg_dict[rv_protocol.KEY_MSG_RAW]
         elif msg_dict[rv_protocol.KEY_MSG_TYPE] == rv_protocol.MSG_TYPE_CMD:
@@ -46,6 +51,7 @@ class RvController(ControlServer.MsgListener):
 
 # main #
 ControlServer.setLoglevel(logging.INFO)
-ctrl = RvController()
-server = ControlServer.Server(ctrl)
-server.serveForever()
+controller = RvController()
+print "starting controller"
+controller.run()
+
